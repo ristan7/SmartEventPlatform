991f38bbb221812bedc2121642e3727fe06f9b13
Mihajlo Ristanovic <mr20220302@student.fon.bg.ac.rs>
2026-03-29 03:23:49 +0200

    Add and refine domain entities with proper relationships

---
+++ SmartEventPlatformWeb/Domains/EventRole.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartEventPlatformWeb.Domains
+{
+    public class EventRole
+    {
+        public long EventRoleId { get; set; }
+        public string Name { get; set; } = string.Empty;
+
+        public ICollection<EventSpeaker> EventSpeakers { get; set; } = new List<EventSpeaker>();
+    }
+}
---
+++ SmartEventPlatformWeb/Domains/EventSpeaker.cs
@@ -0,0 +1,19 @@
+﻿namespace SmartEventPlatformWeb.Domains
+{
+    public class EventSpeaker
+    {
+        public long EventSpeakerId { get; set; }
+        public DateTime Time { get; set; }
+        public string Topic { get; set; } = string.Empty;
+
+        public long EventId { get; set; }
+        public long SpeakerId { get; set; }
+        public long EventRoleId { get; set; }
+
+        public Event? Event { get; set; }
+        public Speaker? Speaker { get; set; }
+        public EventRole? EventRole { get; set; }
+
+
+    }
+}
