0c0b0e80b69b4867ac61facde99dd5151389040c
Mihajlo Ristanovic <mr20220302@student.fon.bg.ac.rs>
2026-03-29 23:56:23 +0200

    Added CRUD for Locations

---
+++ SmartEventPlatformWeb/ViewModels/Locations/LocationDetailsViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace SmartEventPlatformWeb.ViewModels.Locations
+{
+    public class LocationDetailsViewModel
+    {
+        public long LocationId { get; set; }
+        public string LocationName { get; set; } = string.Empty;
+        public string Address { get; set; } = string.Empty;
+        public int Capacity { get; set; }
+    }
+}
